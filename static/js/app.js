6ab6df172634962d62b43de9ff848e9e29f5e54e
John <steskalj@outlook.com>
2021-05-25 15:48:26 -0400

    Assignment Site

---
+++ static/js/app.js
@@ -0,0 +1,71 @@
+let opt;
+function GetData(row){
+    if(+row.id === +opt)
+    {   
+        return row
+    };
+}
+
+function BuildDemo(row){
+    const dInfo = d3.selectAll('#sample-metadata');
+    dInfo.selectAll("*").remove();
+    const keycount = Object.keys(row[0]).length;
+    for(var j =0; j < keycount; j++)
+    {
+        dInfo.append("p")
+        .text(`${Object.keys(row[0])[j]}: ${Object.values(row[0])[j]}`)
+    }
+}
+
+function BuildDash(row){
+    //const dBar = d3.selectAll('#bar');
+    //const dBubble = d3.selectAll('#bubble');
+    let dBar = [{
+        type: 'bar',
+        x: row[0].sample_values.slice(0,10),
+        y: row[0].otu_ids.slice(0,10),
+        text: row[0].otu_labels.slice(0,10),
+        orientation: 'h'
+      }];
+    let dBubble =[{
+        x: row[0].otu_ids.slice(0,10),
+        y: row[0].sample_values.slice(0,10),
+        mode: 'markers',
+        marker: {
+            color: row[0].otu_ids.slice(0,10),
+            opacity: [1, 0.8, 0.6, 0.4],
+            size: row[0].sample_values.slice(0,10)
+        },
+        text: row[0].otu_labels.slice(0,10)    
+    }];
+    Plotly.newPlot('bar', dBar);
+    Plotly.newPlot('bubble', dBubble);
+}
+d3.json('samples.json').then( d => {
+    const dSelect = d3.select('#selDataset');
+    d.names.forEach(d => {
+        const dOpt = dSelect.append("option");
+        dOpt.attr('value',d)
+        .text(d);
+    })
+    optionChanged(d.names[0]);
+})
+
+// d3.select('#selDataset').on('change', d => {
+//     console.log(d.value);
+// })
+
+function optionChanged(o)
+{
+    opt = o;
+    d3.json('samples.json').then( d => {
+        
+        const dMetadata = d.metadata;
+        const dSamples = d.samples;
+        const mRows = dMetadata.filter(GetData);
+        const sRows = dSamples.filter(GetData);
+        BuildDemo(mRows);
+        BuildDash(sRows);
+        
+    })
+}
